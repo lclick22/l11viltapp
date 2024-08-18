a2f884dba859de3dc84888a91666f1ed37e5a208
Santosh Kumar <sankumdev@gmail.com>
2024-08-18 21:14:25 +0530

    Initial commit

---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Index</div>
+    <Link href="/show">Show Page</Link>
+    <div>The message is: {{ message }}</div>
+</template>
+<script setup>
+    import { Link } from '@inertiajs/vue3';
+    defineProps({
+        message: String
+    });
+</script>
+<script>
+    import MainLayout from '../../Layouts/MainLayout.vue';
+    export default {
+        layout: MainLayout
+    }
+</script>
